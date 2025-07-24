27f5f31b3a39f15ba4bc7eac3d63e93df61bf5ab
Github1234fab <fabien.marceau@gmail.com>
2025-07-24 11:41:22 +0200

    ok

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,14 +1,16 @@
-
 import { redirect } from '@sveltejs/kit';
 
-
 export function load({ url }) {
-	const segments = url.pathname.split('/').filter(Boolean);
-	const lang = segments[0];
-  
-	if (!lang || !['fr', 'en'].includes(lang)) {
-	  throw redirect(307, '/fr/home');
-	}
-  
-	return {};
+  try {
+    const segments = url.pathname.split('/').filter(Boolean);
+    const lang = segments[0];
+
+    if (!lang || !['fr', 'en'].includes(lang)) {
+      throw redirect(307, '/fr/home');
+    }
+    return {};
+  } catch (err) {
+    console.error('Erreur dans +layout.server.js load:', err);
+    throw err;  // ou retourne un message d'erreur pour debug
   }
+}
