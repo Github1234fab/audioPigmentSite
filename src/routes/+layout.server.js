ac6adb9520aeb53190c7570018ec0fb069a5f16f
Github1234fab <fabien.marceau@gmail.com>
2025-07-24 11:19:48 +0200

    ok

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,14 +2,13 @@
 import { redirect } from '@sveltejs/kit';
 
 
-
 export function load({ url }) {
 	const segments = url.pathname.split('/').filter(Boolean);
 	const lang = segments[0];
-
-
+  
 	if (!lang || !['fr', 'en'].includes(lang)) {
-		throw redirect(307, '/fr/home');
+	  throw redirect(307, '/fr/home');
 	}
-}
-
+  
+	return {};
+  }
