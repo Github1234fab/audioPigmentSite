0f24025196bccb51707f65b4868028bfd17617a9
Github1234fab <fabien.marceau@gmail.com>
2026-05-22 18:15:50 +0200

    correction div video svelte

--- src/routes/fr/realisations/+page.svelte
+++ src/routes/fr/realisations/+page.svelte
@@ -106,13 +106,20 @@
 <UnderConstructionOverlay lang="fr" />
 
 {#if activeVideo}
-  <div class="modal-backdrop" on:click={closeVideo} transition:fade={{ duration: 200 }}>
+  <div
+    class="modal-backdrop"
+    on:click={closeVideo}
+    on:keydown={(e) => { if (e.key === 'Enter' || e.key === ' ') closeVideo(); }}
+    role="button"
+    tabindex="0"
+    transition:fade={{ duration: 200 }}
+  >
     <button class="close-btn" on:click={closeVideo} aria-label="Close video">
       <svg viewBox="0 0 24 24" width="24" height="24">
         <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" fill="white"/>
       </svg>
     </button>
-    <div class="video-container" on:click|stopPropagation>
+    <div class="video-container" on:click|stopPropagation role="presentation">
       <iframe
         src={activeVideo}
         title="YouTube video player"
